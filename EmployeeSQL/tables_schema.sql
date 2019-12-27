011a160f7cb981caaedd2d10ebdb0f1bd63739c5
Julia O'Brien <jobrien1726@Julias-MacBook-Air.local>
2019-12-26 22:05:11 -0500

    updated png image to match most recent ERD and started .sql file for analysis queries

---
+++ EmployeeSQL/tables_schema.sql
@@ -0,0 +1,47 @@
+CREATE TABLE depts (
+    dept_no VARCHAR PRIMARY KEY,
+    dept_name VARCHAR
+);
+
+CREATE TABLE employees (
+    emp_no INTEGER PRIMARY KEY,
+    birth_date VARCHAR,
+    first_name VARCHAR,
+    last_name VARCHAR,
+    gender VARCHAR(1),
+    hire_date VARCHAR
+);
+
+CREATE TABLE dept_emp (
+    emp_no INTEGER,
+    dept_no VARCHAR,
+    from_date VARCHAR,
+    to_date VARCHAR,
+    FOREIGN KEY (dept_no) REFERENCES depts(dept_no),
+    FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
+
+CREATE TABLE dept_manager (
+    dept_no VARCHAR,
+    emp_no INTEGER,
+    from_date VARCHAR,
+    to_date VARCHAR,
+    FOREIGN KEY (dept_no) REFERENCES depts(dept_no),
+    FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
+
+CREATE TABLE salaries (
+    emp_no INTEGER,
+    salary INTEGER,
+    from_date VARCHAR,
+    to_date VARCHAR,
+    FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
+
+CREATE TABLE titles (
+    emp_no INTEGER,
+    title VARCHAR,
+    from_date VARCHAR,
+    to_date VARCHAR,
+    FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
