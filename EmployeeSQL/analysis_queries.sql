279cf53449f968bb61a7173188ee68291b71c07c
Julia O'Brien <jobrien1726@Julias-MacBook-Air.local>
2020-05-29 12:41:31 -0400

    edited query code and added image of queries

--- EmployeeSQL/analysis_queries.sql
+++ EmployeeSQL/analysis_queries.sql
@@ -1,4 +1,5 @@
--- 1.List the following details of each employee: employee number, last name, first name, gender, and salary.
+-- 1.List the following details of each employee: employee number, last name, 
+-- first name, gender, and salary.
 SELECT e.emp_no, e.last_name, e.first_name, e.gender, s.salary
 FROM employees AS e
 JOIN salaries AS s 
@@ -9,7 +10,8 @@ SELECT *
 FROM employees
 WHERE hire_date LIKE '1986%';
 
---3.List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name, and start and end employment dates.
+--3.List the manager of each department with the following information: department number, 
+-- department name, the manager's employee number, last name, first name, and start and end employment dates.
 SELECT d.dept_no, d.dept_name, dm.emp_no, e.last_name, e.first_name, dm.from_date, dm.to_date
 FROM dept_manager AS dm
 JOIN depts AS d
@@ -17,7 +19,8 @@ ON d.dept_no = dm.dept_no
 JOIN employees AS e
 ON e.emp_no = dm.emp_no;
 
---4.List the department of each employee with the following information: employee number, last name, first name, and department name.
+--4.List the department of each employee with the following information: employee number, 
+-- last name, first name, and department name.
 SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
 FROM dept_emp AS de
 JOIN employees AS e
